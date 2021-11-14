93c8d79b0b0c71a0ca58de580983b8d1687a8d62
kevinmr59 <kevinmura59610@gmail.com>
2021-11-14 14:24:44 +0100

    DOM

---
+++ add.js
@@ -0,0 +1,72 @@
+let oneElement = document.getElementById('one');
+console.log(oneElement);
+oneElement.style.color='red';
+oneElement.style.fontSize='2rem';
+
+
+
+let twoElement = document.getElementById('two');
+console.log(twoElement);
+twoElement.style.color='red';
+twoElement.style.fontSize='2rem'
+
+let sizeName = document.getElementById('two')
+sizeName.style.animationName= 'size';
+let size = document.getElementById('two')
+size.style.animationIterationCount = '2';
+let sizeTime = document.getElementById('two')
+sizeTime.style.animationDuration = '5s';
+let sizeFont = document.getElementById( 'two')
+sizeFont.style.fontSize = '1.8rem';
+
+let threeElement = document.getElementById('three');
+console.log(threeElement);
+threeElement.style.color='red';
+threeElement.style.fontSize='2rem'
+
+let fourElement = document.getElementById('four');
+console.log(fourElement);
+fourElement.style.color='red';
+fourElement.style.fontSize='2rem';
+
+let fiveElement = document.getElementById('five');
+console.log(fiveElement);
+fiveElement.style.color='red';
+fiveElement.style.fontSize='2rem'
+
+let sixElement = document.getElementById('six');
+console.log(sixElement);
+sixElement.style.background='red';
+
+let sevenElement = document.getElementById('seven');
+console.log(sevenElement);
+sevenElement.style.background='red';
+sevenElement.innerText=('helloworld');
+
+let heigtElement = document.getElementById('heigt');
+console.log(heigtElement);
+heigtElement.style.background='red';
+heigtElement.style.direction=('rtl');
+
+let nineElement = document.getElementById('nine');
+console.log(nineElement);
+nineElement.style.background='red';
+
+let tenElement = document.getElementById('ten');
+console.log(tenElement);
+tenElement.style.background='red';
+
+let a1Element= document.getElementById('a1');
+a1Element.innerText=("lorem ipsum");
+
+let a2Element= document.getElementById('a2');
+a2Element.innerText=("lorem ipsum");
+
+let a3Element= document.getElementById('a3');
+a3Element.innerText=("lorem ipsum");
+
+let a4Element= document.getElementById('a4');
+a4Element.innerText=("lorem ipsum");
+
+let a5Element= document.getElementById('a5');
+a5Element.innerText=("lorem ipsum");
